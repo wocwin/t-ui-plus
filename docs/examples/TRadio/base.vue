948fa16bd709bd661a2f126c462126992ec29fe9
jason <1207436809@qq.com>
2023-12-07 00:37:06 +0800

    radio 组件初步封装

--- docs/examples/TRadio/base.vue
+++ docs/examples/TRadio/base.vue
@@ -1,10 +1,20 @@
 <template>
   <t-layout-page class="t_button_demo">
     <t-layout-page-item>
-      <t-radio>导出</t-radio>
+      <t-radio :options="options">导出</t-radio>
     </t-layout-page-item>
   </t-layout-page>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
+const options = ref([
+  {
+    label: '导出',
+    value: 'export',
+  },
+  {
+    label: '导入',
+    value: 'import',
+  },
+])
 </script>
--- packages/radio/src/index.vue
+++ packages/radio/src/index.vue
@@ -1,16 +1,25 @@
 <template>
   <el-radio-group v-model="radio1" v-bind="$attrs">
-    <el-radio label="1" size="large">Option 1</el-radio>
-    <el-radio label="2" size="large">Option 2</el-radio>
+    <component
+      v-for="item in options"
+      :key="item.value"
+      :label="item.value"
+      :is="type === 'radio' ? 'el-radio' : 'el-radio-button'"
+      >{{ item.label }}
+    </component>
   </el-radio-group>
 </template>
 
 <script setup lang="ts" name="TRadio">
 import { ref } from 'vue'
 const props = defineProps({
-  time: {
-    type: Number,
-    default: 1000,
+  type: {
+    type: String,
+    default: 'radio',
+  },
+  options: {
+    type: Array,
+    default: () => [],
   },
 })
 // 抛出事件
